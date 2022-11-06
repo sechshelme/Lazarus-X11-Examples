a98144f4df5c34f1320cd151c8e16609146ef706
sechshelme <m_burkhard@gmx.ch>
2022-11-06 14:09:03 +0100

    Window

---
+++ 01_-_Einfuehrung/15_-_Fenster_im_Fenster/Project1.pas
@@ -0,0 +1,109 @@
+//image image.png
+(*
+Erstes Fenster mit <b>X11</b> wird erstellt.
+Es wird nur ein einziges Eregniss gebraucht.
+Ein Tastatur-Event, welches <b>[ESC]</b> abfängt und das Programm beendet.
+*)
+//lineal
+//code+
+program Project1;
+
+uses
+  unixtype,
+  ctypes,
+  xlib,
+  xutil,
+  keysym,
+  x;
+
+var
+  dis: PDisplay;
+  win, Subwin1, Subwin2: TWindow;
+  Event: TXEvent;
+  scr: cint;
+  gc: TGC;
+  i: integer;
+
+begin
+  // Erstellt die Verbindung zum Server
+  dis := XOpenDisplay(nil);
+  if dis = nil then begin
+    WriteLn('Kann nicht das Display öffnen');
+    Halt(1);
+  end;
+  scr := DefaultScreen(dis);
+  gc := DefaultGC(dis, scr);
+
+
+
+  // Erstellt das Fenster
+  win := XCreateSimpleWindow(dis, RootWindow(dis, scr), 10, 10, 640, 480, 1, BlackPixel(dis, scr), WhitePixel(dis, scr));
+
+  Subwin1 := XCreateSimpleWindow(dis, win, 100, 100, 320, 240, 1, BlackPixel(dis, scr), WhitePixel(dis, scr));
+  Subwin2 := XCreateSimpleWindow(dis, win, 250, 100, 320, 240, 1, BlackPixel(dis, scr), WhitePixel(dis, scr));
+
+  // Wählt die gewünschten Ereignisse aus
+  // Es wird nur das Tastendrückereigniss <b>KeyPressMask</b> gebraucht.
+  XSelectInput(dis, win, KeyPressMask or ExposureMask or PointerMotionMask);
+  XSelectInput(dis, Subwin1, KeyPressMask or ExposureMask or PointerMotionMask);
+  XSelectInput(dis, Subwin2, KeyPressMask or ExposureMask or PointerMotionMask);
+
+  // Fenster anzeigen
+  XMapWindow(dis, win);
+  XMapWindow(dis, Subwin1);
+  XMapWindow(dis, Subwin2);
+
+  // Ereignisschleife
+  while (True) do begin
+    XNextEvent(dis, @Event);
+
+    case Event._type of
+      Expose: begin
+        XClearWindow(dis, win);
+        XClearWindow(dis, Subwin1);
+        for i := 0 to 100 do begin
+          XSetForeground(dis, gc, $FF);
+          XDrawArc(dis, win, gc, random(500) - 200, random(500) - 200, 150, 150, 0, 360 * 64);
+          XClearArea(dis,win,20,20,100,100,False);
+
+          XSetForeground(dis, gc, $FF shl 8);
+          XDrawArc(dis, Subwin1, gc, random(500) - 200, random(500) - 200, 150, 150, 0, 360 * 64);
+
+          XSetForeground(dis, gc, $FF shl 16);
+          XDrawArc(dis, Subwin2, gc, random(500) - 200, random(500) - 200, 150, 150, 0, 360 * 64);
+        end;
+      end;
+      MotionNotify: begin
+        //case Event.xbutton.window of
+        //  win: begin
+        //    WriteLn('move1');
+        //  end;
+        //  Subwin1: begin
+        //    WriteLn('move2');
+        //  end;
+        //end;
+        if Event.xbutton.window = win then begin
+          WriteLn('root');
+        end;
+        if Event.xbutton.window = Subwin1 then begin
+          WriteLn('Sub1');
+        end;
+        if Event.xbutton.window = Subwin2 then begin
+          WriteLn('Sub2');
+        end;
+      end;
+
+      KeyPress: begin
+        // Beendet das Programm bei [ESC]
+        if XLookupKeysym(@Event.xkey, 0) = XK_Escape then begin
+          Break;
+        end;
+      end;
+    end;
+
+  end;
+
+  // Schliesst Verbindung zum Server
+  XCloseDisplay(dis);
+end.
+//code-
---
+++ 01_-_Einfuehrung/25_-_Mehrere _Fenster/Project1.pas
@@ -0,0 +1,70 @@
+//image image.png
+(*
+Erstes Fenster mit <b>X11</b> wird erstellt.
+Es wird nur ein einziges Eregniss gebraucht.
+Ein Tastatur-Event, welches <b>[ESC]</b> abfängt und das Programm beendet.
+*)
+//lineal
+//code+
+program Project1;
+
+uses
+  unixtype,
+  ctypes,
+  xlib,
+  xutil,
+  keysym,
+  x;
+
+var
+  dis: PDisplay;
+  win1,win2 :TWindow;
+  Event: TXEvent;
+  scr: cint;
+
+begin
+  // Erstellt die Verbindung zum Server
+  dis := XOpenDisplay(nil);
+  if dis = nil then begin
+    WriteLn('Kann nicht das Display öffnen');
+    Halt(1);
+  end;
+  scr := DefaultScreen(dis);
+
+  // Erstellt das Fenster
+  win1 := XCreateSimpleWindow(dis, RootWindow(dis, scr), 10, 10, 320, 240, 1, BlackPixel(dis, scr), WhitePixel(dis, scr));
+//  win2 := XCreateSimpleWindow(dis, RootWindow(dis, scr), 10, 10, 320, 240, 1, BlackPixel(dis, scr), WhitePixel(dis, scr));
+  win2 := XCreateSimpleWindow(dis, win1, 10, 10, 320, 240, 1, BlackPixel(dis, scr), WhitePixel(dis, scr));
+
+  // Wählt die gewünschten Ereignisse aus
+  // Es wird nur das Tastendrückereigniss <b>KeyPressMask</b> gebraucht.
+  XSelectInput(dis, win1, KeyPressMask or ExposureMask);
+  XSelectInput(dis, win2, KeyPressMask or ExposureMask);
+
+  // Fenster anzeigen
+  XMapWindow(dis, win1);
+  XMapWindow(dis, win2);
+
+  // Ereignisschleife
+  while (True) do begin
+    XNextEvent(dis, @Event);
+
+    case Event._type of
+      Expose:begin
+//        XSetForeground(dis, gc, Random(Random($FF));
+        end;
+      KeyPress: begin
+        // Beendet das Programm bei [ESC]
+        if XLookupKeysym(@Event.xkey, 0) = XK_Escape then begin
+          Break;
+        end;
+      end;
+    end;
+
+  end;
+
+  // Schliesst Verbindung zum Server
+  XCloseDisplay(dis);
+end.
+//code-
+
