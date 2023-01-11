9869af2fa520409dd71e34afbe25b75d103beb4e
sechshelme <m_burkhard@gmx.ch>
2023-01-11 15:32:40 +0100

    athena

--- Code-Blocks/Athena/xt_Xaw_test_5/main.c
+++ Code-Blocks/Athena/xt_Xaw_test_5/main.c
@@ -1,6 +1,6 @@
 /*
- * Компилируется строкой cc -o simple simple.c -lXaw -lXt -lX11
- */
+    Компилируется строкой cc -o simple simple.c -lXaw -lXt -lX11
+*/
 #include <X11/Intrinsic.h>
 #include <X11/StringDefs.h>
 #include <X11/Shell.h>
@@ -28,21 +28,21 @@ Window wchild;
 // Button 3 makes the Popup short and wide
 void popevent(Widget w, XtPointer p, XEvent *event, char* CTmp)
 {
-if (event->xbutton.button == Button1)
-{
-fprintf(stderr, "Child Button 1 Pressed\n");
-XtPopdown(widget_child);
-}
-if (event->xbutton.button == Button2)
-{
-fprintf(stderr, "Child Button 2 Pressed\n");
-XtResizeWidget(widget_child, 120, 350, 10);
-}
-if (event->xbutton.button == Button3)
-{
-fprintf(stderr, "Child Button 3 Pressed\n");
-XtResizeWidget(widget_child, 200, 50, 10);
-}
+    if (event->xbutton.button == Button1)
+    {
+        fprintf(stderr, "Child Button 1 Pressed\n");
+        XtPopdown(widget_child);
+    }
+    if (event->xbutton.button == Button2)
+    {
+        fprintf(stderr, "Child Button 2 Pressed\n");
+        XtResizeWidget(widget_child, 120, 350, 10);
+    }
+    if (event->xbutton.button == Button3)
+    {
+        fprintf(stderr, "Child Button 3 Pressed\n");
+        XtResizeWidget(widget_child, 200, 50, 10);
+    }
 }
 //------------------------------------------------
 // This function is called by a ButtonPress event in the main widget
@@ -51,59 +51,73 @@ XtResizeWidget(widget_child, 200, 50, 10);
 // Button 3 quits the program
 void EvProc(Widget w, XtPointer client_data, XEvent *event, char* CTmp)
 {
-Colormap cmap;
-XColor color;
-if (event->xbutton.button == Button1)
-{
-fprintf(stderr, "Button 1 Pressed\n");
-if (widget_child == NULL)
-{
-widget_child = XtCreatePopupShell("Popup Demo", applicationShellWidgetClass, main_widget, NULL, 0);
-XtAddEventHandler(widget_child, ButtonPressMask, FALSE, popevent, NULL);
-}
-XtResizeWidget(widget_child, 200, 50, 10);
-XtPopup(widget_child, XtGrabNone); //--- Save child widget window & display values
-wchild = XtWindow(widget_child);
-dchild = XtDisplay(widget_child);
-cmap = DefaultColormap(dchild, DefaultScreen(dchild));
-if (XParseColor(dchild, cmap, "light cyan", &color) == 0) fprintf(stderr, "Bad Parse\n");
-if (XAllocColor(dchild, cmap, &color) == 0) fprintf(stderr, "Bad Color");
-XSetWindowBackground(dchild, wchild, color.pixel);
-XClearWindow(dchild, wchild);
-}
-if (event->xbutton.button == Button2) fprintf(stderr, "Button 2 Pressed\n");
-if (event->xbutton.button == Button3)
-{
-fprintf(stderr, "Button 3 Pressed\n");
-exit(0);
-}
+    Colormap cmap;
+    XColor color;
+    if (event->xbutton.button == Button1)
+    {
+        fprintf(stderr, "Button 1 Pressed\n");
+        if (widget_child == NULL)
+        {
+            widget_child = XtCreatePopupShell("Popup Demo", applicationShellWidgetClass, main_widget, NULL, 0);
+            XtAddEventHandler(widget_child, ButtonPressMask, FALSE, popevent, NULL);
+        }
+        XtResizeWidget(widget_child, 200, 50, 10);
+        XtPopup(widget_child, XtGrabNone); //--- Save child widget window & display values
+        wchild = XtWindow(widget_child);
+        dchild = XtDisplay(widget_child);
+        cmap = DefaultColormap(dchild, DefaultScreen(dchild));
+        if (XParseColor(dchild, cmap, "light cyan", &color) == 0)
+        {
+            fprintf(stderr, "Bad Parse\n");
+        }
+        if (XAllocColor(dchild, cmap, &color) == 0)
+        {
+            fprintf(stderr, "Bad Color");
+        }
+        XSetWindowBackground(dchild, wchild, color.pixel);
+        XClearWindow(dchild, wchild);
+    }
+    if (event->xbutton.button == Button2)
+    {
+        fprintf(stderr, "Button 2 Pressed\n");
+    }
+    if (event->xbutton.button == Button3)
+    {
+        fprintf(stderr, "Button 3 Pressed\n");
+        exit(0);
+    }
 }
 //------------------------------------------------
 void main (int argc, char **argv)
 {
-short unsigned int xx,yy;
-Colormap cmap;
-XColor color;
-widget_child = NULL;
+    short unsigned int xx,yy;
+    Colormap cmap;
+    XColor color;
+    widget_child = NULL;
 //--- Create and initialize the top-level widget
-main_widget = XtOpenApplication(&app, "My Application", NULL, 0, &argc, argv, NULL, applicationShellWidgetClass, NULL, 0);
+    main_widget = XtOpenApplication(&app, "My Application", NULL, 0, &argc, argv, NULL, applicationShellWidgetClass, NULL, 0);
 //--- Make it the desired size
-XtMakeResizeRequest(main_widget, 300, 100, &xx, &yy);
+    XtMakeResizeRequest(main_widget, 300, 100, &xx, &yy);
 //--- Realize the main widget
-XtRealizeWidget(main_widget);
+    XtRealizeWidget(main_widget);
 
 //Save main widget Window & Display values
-main_wind = XtWindow(main_widget);
-main_disp = XtDisplay(main_widget);
+    main_wind = XtWindow(main_widget);
+    main_disp = XtDisplay(main_widget);
 //--- Load default colormap and allocate a color close to the desired background color
-cmap = DefaultColormap(main_disp, DefaultScreen(main_disp));
-if (XParseColor(main_disp, cmap, "dark_blue", &color) == 0) fprintf(stderr, "Bad Parse\n");
-if (XAllocColor(main_disp, cmap, &color) == 0) fprintf(stderr, "Bad Color\n");
-XSetWindowBackground(main_disp, main_wind, color.pixel);
+    cmap = DefaultColormap(main_disp, DefaultScreen(main_disp));
+    if (XParseColor(main_disp, cmap, "dark_blue", &color) == 0)
+    {
+        fprintf(stderr, "Bad Parse\n");
+    }
+    if (XAllocColor(main_disp, cmap, &color) == 0)
+    {
+        fprintf(stderr, "Bad Color\n");
+    }
+    XSetWindowBackground(main_disp, main_wind, color.pixel);
 //--- Add Mouse Button Event Handler
-XtAddEventHandler(main_widget, ButtonPressMask, FALSE, EvProc, NULL);
+    XtAddEventHandler(main_widget, ButtonPressMask, FALSE, EvProc, NULL);
 //-----
-XClearWindow(main_disp, main_wind);
-XtAppMainLoop(app);
-
+    XClearWindow(main_disp, main_wind);
+    XtAppMainLoop(app);
 }
