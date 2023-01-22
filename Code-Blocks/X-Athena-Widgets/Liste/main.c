c843d4faf371b4f60950f51615821643a5e1fefa
sechshelme <m_burkhard@gmx.ch>
2023-01-22 18:14:17 +0100

    list

---
+++ Code-Blocks/X-Athena-Widgets/Liste/main.c
@@ -0,0 +1,43 @@
+// https://ergodic.ugr.es/cphys_pedro/unix/athena2.html
+
+#include  <X11/StringDefs.h>
+#include  <X11/Intrinsic.h>
+#include  <X11/Xaw/Box.h>
+#include  <X11/Xaw/Command.h>
+#include  <X11/Xaw/List.h>
+#include  <stdio.h>
+
+void hello(Widget w, XtPointer *client, XtPointer call) {
+    printf("Hello World\n");
+}
+
+void quit(Widget w, XtPointer *client, XtPointer call) {
+    exit(0);
+}
+
+int main(int argc, char **argv) {
+    Widget toplevel, box, command, l;
+
+    toplevel = XtInitialize("Mein Fenster", "simple", NULL, 0, &argc, argv);
+
+    box = XtCreateManagedWidget("Button Gruppe", boxWidgetClass, toplevel, NULL, 0);
+
+    command = XtCreateManagedWidget("Hello World !", commandWidgetClass, box, NULL, 0);
+    XtAddCallback(command, XtNcallback, hello, NULL);
+
+    command = XtCreateManagedWidget("Programm beenden", commandWidgetClass, box, NULL, 0);
+    XtAddCallback(command, XtNcallback, quit, NULL);
+
+
+
+    char **ch={"abc","def","ghi","jkl",0};
+
+    l=XtCreateManagedWidget("Liste", listWidgetClass, box, NULL, 0);
+    XawListChange(l, ch, 4, 0, True);
+
+    XtRealizeWidget(toplevel);
+
+    XtMainLoop();
+}
+
+
