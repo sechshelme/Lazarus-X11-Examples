020f37c8b03dd4fba6c8745cb07981ea48a75b28
sechshelme <tux@tux-B660M-DS3H-DDR4>
2023-06-06 19:40:36 +0200

    fontinfo

--- 87_-_Diverses/80_-_Font_Info/Project1.pas
+++ 87_-_Diverses/80_-_Font_Info/Project1.pas
@@ -9,7 +9,7 @@ Ein Tastatur-Event, welches <b>[ESC]</b> abfängt und das Programm beendet.
 program Project1;
 
 uses
-  sysutils,
+  SysUtils,
   unixtype,
   ctypes,
   xlib,
@@ -23,34 +23,107 @@ var
   Event: TXEvent;
   scr: cint;
 
+  // https://www.x.org/releases/X11R7.7/doc/xorg-docs/xlfd/xlfd.html
+
+const
+  ListAtoms: TStringArray = (
+    'FOUNDRY',
+    'FAMILY_NAME',
+    'WEIGHT_NAME',
+    'SLANT',
+    'SETWIDTH_NAME',
+    'ADD_STYLE_NAME',
+    'SPACING',
+    'CHARSET_REGISTRY',
+    'CHARSET_ENCODING',
+    'FACE_NAME',
+    'COPYRIGHT',
+    'NOTICE',
+    'FONT',
+    'FONT_NAME');
+
+  ListCardinal: TStringArray = (
+    'PIXEL_SIZE',
+    'POINT_SIZE',
+    'RESOLUTION_X',
+    'RESOLUTION_Y',
+    'AVERAGE_WIDTH',
+    'RESOLUTION',
+    'MIN_SPACE',
+    'NORM_SPACE',
+    'MAX_SPACE',
+    'END_SPACE',
+    'RELATIVE_SETWIDTH',
+    'RELATIVE_WEIGHT',
+    'CAP_HEIGHT',
+    'SUPERSCRIPT_SIZE',
+    'SUBSCRIPT_SIZE',
+    'DESTINATION');
+
+  ListInt: TStringArray = (
+    'QUAD_WIDTH',
+    'SUPERSCRIPT_X',
+    'SUPERSCRIPT_Y',
+    'SUBSCRIPT_X',
+    'SUBSCRIPT_Y',
+    'UNDERLINE_POSITION',
+    'UNDERLINE_THICKNESS',
+    'STRIKEOUT_ASCENT',
+    'STRIKEOUT_DESCENT',
+    'ITALIC_ANGLE',
+    'X_HEIGHT',
+    'WEIGHT',
+    'AVG_CAPITAL_WIDTH',
+    'AVG_LOWERCASE_WIDTH',
+    'FIGURE_WIDTH',
+    'SMALL_CAP_SIZE');
+
+  function Is_Atom(Src: string; list: TStringArray): boolean;
+  var
+    i: integer;
+    s: string;
+  begin
+    Result := False;
+    for i := 0 to Length(list) - 1 do begin
+      s := string(Src);
+      if list[i] = s then begin
+        Result := True;
+        Exit;
+      end;
+    end;
+  end;
+
   procedure Print_FontInfo;
   var
     i: integer;
     font: PXFontStruct;
-    font_name: TAtom;
+    font_prop_Atom: TAtom;
     fontprop: culong;
-    pc: PChar;
+    pc, font_prop_Name: PChar;
   begin
 
-
     font := XLoadQueryFont(dis, '-bitstream-bitstream charter-medium-r-normal--0-0-0-0-p-0-adobe-standard');
-//    font := XLoadQueryFont(dis, 'rk24');
+    //    font := XLoadQueryFont(dis, 'rk24');
     if font = nil then begin
       WriteLn('Konnte Font nicht laden !');
     end;
     WriteLn('count: ', font^.n_properties);
     for i := 0 to font^.n_properties - 1 do begin
-      font_name:=font^.properties[i].Name;
-      fontprop:=font^.properties[i].card32;
-      Write(XGetAtomName(dis, font_name),': ');
-      WriteLn(fontprop,'  ',fontprop.ToHexString);
+      font_prop_Atom := font^.properties[i].Name;
+      font_prop_Name := XGetAtomName(dis, font_prop_Atom);
+      fontprop := font^.properties[i].card32;
+      Write(font_prop_Name, ': ');
+      if Is_Atom(font_prop_Name, ListAtoms) then begin
+        WriteLn(XGetAtomName(dis, fontprop));
+      end else if Is_Atom(font_prop_Name, ListCardinal) then begin
+        WriteLn('cardinal: ',fontprop);
+      end else if Is_Atom(font_prop_Name, ListInt) then begin
+        WriteLn('int: ',fontprop);
+      end else begin
+        WriteLn('unknow: ',fontprop );
+      end;
+//      WriteLn(fontprop, '  ', fontprop.ToHexString);
     end;
-
-//    fontprop:=font^.properties[0].card32;
-
-//         pc:=PChar(@fontprop);
-//   WriteLn(pc);
-
   end;
 
 begin
