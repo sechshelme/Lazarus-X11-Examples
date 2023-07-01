27cc9ef47ecba1918dcc430d69263e6fde4618d9
sechshelme <tux@tux-B660M-DS3H-DDR4>
2023-07-01 19:34:14 +0200

    clipboard

--- 86_-_Atom/05_-_Show_Targets/Project1.pas
+++ 86_-_Atom/05_-_Show_Targets/Project1.pas
@@ -275,7 +275,6 @@ var
     WriteLn();
     if targetAtom <> 0 then begin
       WriteLn('prop: ', targetAtom, '  name: ', XGetAtomName(dis, targetAtom));
-      //      XGetWindowProperty(dis, w, XA_CLIPBOARD, 0, MaxInt, False, 0, @ret_type, @ret_format, @ret_items, @ret_bytesleft, @prop_return);
       XGetWindowProperty(dis, w, XA_CLIPBOARD, 0, MaxInt, True, 0, @ret_type, @ret_format, @ret_items, @ret_bytesleft, @prop_return);
 
       if ret_items <> 0 then begin
@@ -285,11 +284,6 @@ var
         end;
         Inc(ClipData.ofs,ret_items);
       end else ClipData.INCR:=False;
-
-      WriteLn('ofs: ', ClipData.ofs);
-      WriteLn('len: ',Length(ClipData.Data));
-
-      writeln('--------------- ret:', ret_items);
 
       if ret_type = 0 then begin
         WriteLn(#10'Unbekannt !');
@@ -340,41 +334,31 @@ var
             if i = 0 then begin
               Write('"');
             end;
-//            ch := PChar(prop_return)[i];
             ch := ClipData.Data[i];
             if (ch = #0) and (i <> ret_items - 1) then begin
               Write('", "');
             end else begin
-//              Write(PChar(prop_return)[i]);
               Write(ClipData.Data[i]);
             end;
           end;
           WriteLn('"');
         end else if ret_type = GetAtom('MULTIPLE') then  begin
           for i := 0 to ret_items - 1 do begin
-//            ch := PChar(prop_return)[i];
             ch := ClipData.Data[i];
             if (ch = #0) and (i <> ret_items - 1) then begin
               Write('", "');
             end else begin
-//              Write(PChar(prop_return)[i]);
               Write(ClipData.Data[i]);
             end;
           end;
           WriteLn('"');
         end else if ret_type = GetAtom('image/bmp') then  begin
-//          ShowBMP(PChar(prop_return), ret_items);
-//          save(PChar(prop_return), ret_items, 'test.bmp');
           save(PChar(ClipData.Data), Length(ClipData.Data), 'test.bmp');
           ShowBMP(PChar( ClipData.Data), Length(ClipData.Data));
         end else if ret_type = GetAtom('image/x-bmp') then  begin
-//          ShowBMP(PChar(prop_return), ret_items);
-  //        save(PChar(prop_return), ret_items, 'test-x.bmp');
           save(PChar(ClipData.Data), Length(ClipData.Data), 'test-x.bmp');
           ShowBMP(PChar( ClipData.Data), Length(ClipData.Data));
         end else if ret_type = GetAtom('image/png') then  begin
-//          ShowPNG(PChar(prop_return), ret_items);
-//          save(PChar(prop_return), ret_items, 'test.png');
           save(PChar(ClipData.Data), Length(ClipData.Data), 'test.png');
           ShowPNG(PChar( ClipData.Data), Length(ClipData.Data));
         end else begin
@@ -441,6 +425,9 @@ begin
             ClipData.ofs := 0;
             ClipData.Data := nil;
             Read_Property(win, Event.xselection.target);
+//            ClipData.INCR := False;
+            ClipData.ofs := 0;
+             ClipData.Data := nil;
           end;
         end;
       end;
